54d55b015582d5800958418371a610b77d95c757
gabo1901 <gabrielvega888@gmail.com>
2022-07-02 13:36:54 -0600

    first commit

---
+++ js/index.js
@@ -0,0 +1,19 @@
+'use strict'
+
+// Cambio de cursor
+
+var cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute('style', 'top: '+(e.pageY - 10)+'px; left: '+(e.pageX - 10)+'px;')
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+
+        
+    }, 500)
+}) 
